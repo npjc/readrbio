7c7f3b0a7a1723bb17d6cc00135318a204c078c2
Nicolas Coutin <nicolas.coutin@gmail.com>
2015-08-28 10:53:15 -0700

    export ls_read and hack document it.

--- R/ls_read.R
+++ R/ls_read.R
@@ -1,16 +1,23 @@
-# read data ---------------------------------------------------------------
-
-# give a
-#' pattern to recognize files by and the
-#' path where to look for the files
-# and ls_read will read in each file with the appropriate reader into
-# a list.
-ls_read <- function(pattern = c(".gff3",".wig"), path = ".", bind_rows = T) {
+#' Read a list of files of the same format based on a pattern and path. 
+#' Optionally bind them together, adding a variable containing the filename
+#' for each observation.
+#' 
+#' @param ptrn character. pattern by which to look up files. defaults to gff3.
+#' @param path character. path to directory to look for files in. 
+#'  defaults to current.
+#' @param bind_rows logical. should all the resulting dfs be bound into one?
+#' @examples
+#'  \dontrun{
+#'  ls_read(".gff3", "./data-raw")
+#'  }
+#' @export
+ls_read <- function(ptrn = c(".gff3",".wig"), path = ".", bind_rows = T) {
+  ptrn <- match.arg(ptrn)
   # get all the files at that place
-  files <- list.files(path, pattern)
+  files <- list.files(path, ptrn)
   file_paths <- normalizePath(paste(path,files, sep = '/'))
   # pick fxn to read based on file extension
-  readfun <- switch(pattern, ".gff3" = read_gff3, ".wig" = read_wig)
+  readfun <- switch(ptrn, ".gff3" = read_gff3, ".wig" = read_wig)
   # read in the files, each one into a list element
   ls <- lapply(file_paths, readfun)
   # Map the file name onto a file_name column for each respective file.
